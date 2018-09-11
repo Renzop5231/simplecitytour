cc87e782043233377dc9019f86cd411ee3f1658c
renzop5231 <renzop5231@gmail.com>
2018-09-11 10:37:25 -0700

    change dependancies

--- restapi/urls.py
+++ restapi/urls.py
@@ -14,23 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include
-from django.urls import path
+from django.conf.urls import include, url
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 # from .views import Tour_cities
 from . import views
 
 urlpatterns = [
-    path('login/', obtain_jwt_token),
-    path('verify_token/', verify_jwt_token),
-    path('logout/', refresh_jwt_token),
+    url('login/', obtain_jwt_token),
+    url('verify_token/', verify_jwt_token),
+    url('logout/', refresh_jwt_token),
     # path('testapi/', views.test_resp, name='testresp'),
-    path('signup/', views.signup_user, name='login'),
-    path('getaudio/', views.get_audio, name='audio'),
-    path('get_imgs/', views.get_cities_imgs, name='imgs'),
-    path('check_update/', views.check_sequence, name='check-update'),
-    path('get_points/', views.get_points, name='points'),
-    path('get_all_locations/', views.get_all_locations, name='get-locations'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('types/', views.getPointTypes, name='check-types'),
+    url('signup/', views.signup_user, name='login'),
+    url('getaudio/', views.get_audio, name='audio'),
+    url('get_imgs/', views.get_cities_imgs, name='imgs'),
+    url('check_update/', views.check_sequence, name='check-update'),
+    url('get_points/', views.get_points, name='points'),
+    url('get_all_locations/', views.get_all_locations, name='get-locations'),
+    url('accounts/', include('django.contrib.auth.urls')),
+    url('types/', views.getPointTypes, name='check-types'),
 ]
